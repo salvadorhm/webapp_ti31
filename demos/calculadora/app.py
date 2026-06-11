86fb0635f1afb7a734ce165eee03afa7048d2b10
Salvador HM <salvadorhm@gmail.com>
2026-06-11 14:07:43 +0000

    UPDATED calculadora

--- demos/calculadora/app.py
+++ demos/calculadora/app.py
@@ -14,12 +14,32 @@ class Index:
 
 class Calculadora:
     def GET(self):
-        return render.calculadora()
+        numero_1 = 0
+        numero_2 = 0
+        resultado = 0
+        return render.calculadora(numero_1, numero_2, resultado)
     
     def POST(self):
         formulario = web.input()
-        numero_1 = formulario['numero_1']
-        return numero_1
+        numero_1 = int(formulario['numero_1'])
+        numero_2 = int(formulario['numero_2'])
+        operacion = formulario['operacion']
+
+        # TODO: programar la operación sumar
+        # TODO: programar la operación restar
+        # TODO: programar la operación dividir
+        # TODO: programar la operación multiplicar
+        # TODO: programar la operación raiz cuadrada al numero_1
+        # TODO: programar la operación potencia numero_1 ** numero_2
+        # TODO: programar la operación modulo
+        # TODO: programar la operación limpiar los valores
+
+        resultado =  numero_1 + numero_2
+
+        print(f"El tipo de dato es: {type(numero_1)}")
+
+        return f"Formulario: {formulario}"
+        #return render.calculadora(numero_1, numero_2, resultado)
 
 if __name__ == "__main__":
     app.run()
